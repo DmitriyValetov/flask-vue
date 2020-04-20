a8b291be097d65c700729c59b46749699429e507
DmitriyValetov <dmitriy.valetov@phystech.edu>
2020-04-20 12:10:56 +0300

    added xhr to flask backend via axios

--- client/src/components/Ping.vue
+++ client/src/components/Ping.vue
@@ -5,12 +5,31 @@
 </template>
 
 <script>
+
+import axious from 'axios';
+
 export default {
   name: 'Ping',
   data() {
     return {
       msg: 'Hello!',
     };
+  },
+  methods: {
+    getMessage() {
+      const path = 'http://localhost:5000/ping';
+      axious.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getMessage();
   },
 };
 </script>
